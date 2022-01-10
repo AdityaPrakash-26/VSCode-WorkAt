42fce52a3eccfdc616ce8c2173f9aa75fa24500f
Aditya Prakash <prakashadityaap@gmail.com>
2022-01-10 12:42:33 +0530

    Added Topics, Companies, and Lists

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const axios = require('axios');
+// const workAtChannel = require('src/workAtChannel.js');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,7 +11,6 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-workat" is now active!');
@@ -24,7 +25,43 @@ function activate(context) {
 		vscode.window.showInformationMessage('Hello World from VSCode-Workat!');
 	});
 
+	// new command to get problems from workat
+	let disposable2 = vscode.commands.registerCommand('vscode-workat.getProblems', function () {
+		try{
+			// show quickpick
+			vscode.window.showQuickPick(['Topics', 'Companies', 'Lists'], {
+				placeHolder: 'Select a list to get problems from workat'
+			}).then(function(selection){
+				// make a get request to https://workat.tech/api/ps/{selection}
+				axios.get('https://workat.tech/api/ps/' + selection)
+					.then(function(response){
+						// add all response.title to quickpick
+						let topics = response.data.map(function(topic){
+							return topic.title;
+						}
+						);
+						vscode.window.showQuickPick(topics, {
+							placeHolder: 'Select a topic to get problems'
+						}).then(function(selection){
+							console.log(selection);
+						});
+					})
+					.catch(function (error) {
+						console.log(error);
+					});
+			});
+		} catch (e) {
+			// create a new output channel
+			const workAtChannel = vscode.window.createOutputChannel('workAt');
+			// log the error into the output channel
+			workAtChannel.appendLine(e.message);
+			// prompt to open the output channel
+			vscode.window.showInformationMessage('Initialization failed. Please open the output channel for more details.');
+		}
+	})
+
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable2);
 }
 
 // this method is called when your extension is deactivated
